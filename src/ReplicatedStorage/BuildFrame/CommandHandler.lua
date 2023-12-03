be1abc55109175b69eaf7f7501295d44a60c90e2
Ncursor <crimsongamingqwerty@gmail.com>
2023-12-02 22:18:16 -0800

    Command 2

--- src/ReplicatedStorage/BuildFrame/CommandHandler.lua
+++ src/ReplicatedStorage/BuildFrame/CommandHandler.lua
@@ -1,3 +1,6 @@
+
+-- !PART OF THE CORE SCRIPT, ONLY EDIT IF YOU KNOW WHAT YOU'RE DOING!
+
 type Command = {Aliases:{string},Permission:number,Function:(PlayerRan:Player,Arguments:{string})->()}
 local Players = game:GetService("Players")
 local Commandlist:{[string]:Command} = require(script.Parent.Commands)
@@ -5,5 +8,11 @@ local Commandlist:{[string]:Command} = require(script.Parent.Commands)
 Players.PlayerAdded:Connect(function(player)
     player.Chatted:Connect(function(message:string)
         local Command = message:split("/")
+        for n,c in Commandlist do
+            if table.find(c.Aliases,Command) then
+                table.remove(Command,1)
+                c.Function(player,Command)
+            end
+        end
     end)
 end)
