d0ac93575eda6e076180667506264fcd9ebadfbf
Ncursor <crimsongamingqwerty@gmail.com>
2023-12-13 19:55:18 -0800

    Giving Metatable for ModInstances

--- src/ReplicatedStorage/BuildFrame/ModSetup.lua
+++ src/ReplicatedStorage/BuildFrame/ModSetup.lua
@@ -1,11 +1,23 @@
-export type ModInstance = {Description:string,Version:number,Credit:string}
+export type ModInstance = {Name:string,Description:string,Version:number,Credit:string}
 export type ModInstanceInfo = (self:any,Name:string,Description:string,Version:number,Credit:string)->ModInstance
 export type ModSetup = {New:ModInstanceInfo & (self:any,mymod:ModuleScript)->ModInstance}
 local ModSetup:ModSetup = {}
+local MetaTemplate = {
+    __tostring = function(self:ModInstance)
+        return `{self.Name}:{self.Version}`
+    end,
+    __eq = function(self:ModInstance,value:ModInstance)
+        if not tostring(value) == `{self.Name}:{self.Version}` then return end
+        return self.Name == value.Name
+    end
+}
 local Data = require(script.Parent.Data)
 local function NewWithTable(Name:string,mymod:ModuleScript)
     local mymodinfo = require(mymod)
-    local ModInstance:ModInstance = {Name = Name,Description = mymodinfo.description,Version = mymodinfo.v,Credit = mymodinfo.credit}
+    local ModInstance = setmetatable({Name = Name,
+    Description = mymodinfo.description,
+    Version = mymodinfo.v,
+    Credit = mymodinfo.credit},MetaTemplate)
     return ModInstance
 end
 function ModSetup:New(Name,Description,Credit,Version)
@@ -18,6 +30,10 @@ function ModSetup:New(Name,Description,Credit,Version)
     Data:NewDatabase(ModInstance.Name)
     Data:SetData("Data",ModInstance,ModInstance.Name)
     return ModInstance
+end
+
+function ModSetup:CreateConfig(ModInstance:ModInstance)
+    
 end
 
 return ModSetup
